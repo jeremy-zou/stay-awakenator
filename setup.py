21116de5f044f8165b9819a80d2e99ab4d411f20
Jeremy Zou <jeremy.zou@gmail.com>
2019-06-28 00:58:47 -0400

    Created setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='stay-awakenator',
+    version='1.0',
+    packages=[''],
+    url='https://github.com/jeremy-zou/stay-awakenator',
+    license='',
+    author='Jeremy Zou',
+    author_email='',
+    description='', install_requires=['imutils', 'dlib', 'opencv-python', 'scipy']
+)
