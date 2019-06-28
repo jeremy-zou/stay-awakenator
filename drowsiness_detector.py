bb5aae16391fea4c8bac2ca4815d42b9dd4f8170
Jeremy Zou <jeremy.zou@gmail.com>
2019-06-27 23:17:25 -0400

    Implemented drowsiness detection

--- drowsiness_detector.py
+++ drowsiness_detector.py
@@ -1,17 +1,79 @@
-import numpy as np
-import argparse
 import imutils
-import time
 import dlib
 import cv2
 from scipy.spatial import distance as dist
 from imutils import face_utils
 from imutils.video import VideoStream
-from threading import Thread
 
 
-def activate():
-    print("ACTIVATED")
+EYE_ASPECT_RATIO_THRESHOLD = 0.25
+CLOSED_EYE_FRAMES_THRESHOLD = 180
+
+
+def main():
+    # initialize dlib's facial detector
+    face_detector = dlib.get_frontal_face_detector()
+
+    # create face shape detector
+    shape_predictor = dlib.shape_predictor("./models/shape_predictor_68_face_landmarks.dat")
+
+    # grab the indexes of the facial landmarks for the left and right eye
+    (l_start, l_end) = face_utils.FACIAL_LANDMARKS_IDXS["left_eye"]
+    (r_start, r_end) = face_utils.FACIAL_LANDMARKS_IDXS["right_eye"]
+
+    # start the video stream
+    print("[INFO] starting video stream thread...")
+    vs = VideoStream().start()
+
+    closed_frames = 0
+    while True:
+        # get frame from video stream
+        frame = vs.read()
+        # resize frame
+        frame = imutils.resize(frame, width=500)
+        gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        # detect faces
+        faces = face_detector(gray_frame, 0)
+
+        for count, face in enumerate(faces):
+            # find facial landmarks
+            shape = shape_predictor(gray_frame, face)
+            # convert landmark locations to numpy array
+            shape = face_utils.shape_to_np(shape)
+
+            # get coordinates for eyes
+            left_eye = shape[l_start:l_end]
+            right_eye = shape[r_start:r_end]
+
+            # get convex hull for eyes and draw on frame
+            left_eye_hull = cv2.convexHull(left_eye)
+            right_eye_hull = cv2.convexHull(right_eye)
+            cv2.drawContours(frame, [left_eye_hull], -1, (255, 0, 0), 1)
+            cv2.drawContours(frame, [right_eye_hull], -1, (255, 0, 0), 1)
+
+            # get EAR for both eyes
+            left_eye_aspect_ratio = eye_aspect_ratio(left_eye)
+            right_eye_aspect_ratio = eye_aspect_ratio(right_eye)
+            ear = (left_eye_aspect_ratio + right_eye_aspect_ratio) / 2.0
+
+            # if eyes are smaller than threshold
+            if ear < EYE_ASPECT_RATIO_THRESHOLD:
+                closed_frames += 1
+                # if eyes are smaller than threshold for longer than predefined amount of frames
+                if closed_frames >= CLOSED_EYE_FRAMES_THRESHOLD:
+                    cv2.putText(frame, "DETECTED DROWSINESS", (10, 30), cv2.FONT_HERSHEY_PLAIN, 1.0, (0, 0, 255), 2)
+            # if eyes are larger than threshold
+            else:
+                closed_frames = 0
+
+            cv2.putText(frame, "Eye Aspect Ratio: {:.2f}".format(ear), (300, 30), cv2.FONT_HERSHEY_PLAIN, 1.0,
+                        (0, 255, 0), 2)
+
+        # show each frame for 1ms
+        cv2.imshow("Frame", frame)
+        keypress = cv2.waitKey(1)
+        if keypress == ord('q'):
+            break
 
 
 def eye_aspect_ratio(eye):
@@ -31,49 +93,5 @@ def eye_aspect_ratio(eye):
     return ear
 
 
-# initialize dlib's facial detector
-face_detector = dlib.get_frontal_face_detector()
-
-# create face shape detector
-shape_predictor = dlib.shape_predictor("./models/shape_predictor_68_face_landmarks.dat")
-
-# grab the indexes of the facial landmarks for the left and right eye
-(l_start, l_end) = face_utils.FACIAL_LANDMARKS_IDXS["left_eye"]
-(r_start, r_end) = face_utils.FACIAL_LANDMARKS_IDXS["right_eye"]
-
-# start the video stream
-print("[INFO] starting video stream thread...")
-vs = VideoStream().start()
-
-while True:
-    # get frame from video stream
-    frame = vs.read()
-    # resize frame
-    # frame = imutils.resize(frame, width=500)
-    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    # detect faces
-    faces = face_detector(gray_frame, 0)
-
-    for count, face in enumerate(faces):
-        # find facial landmarks
-        shape = shape_predictor(gray_frame, face)
-        # convert landmark locations to numpy array
-        shape = face_utils.shape_to_np(shape)
-        # get coordinates for eyes
-        left_eye = shape[l_start:l_end]
-        right_eye = shape[r_start:r_end]
-        # get convex hull for eyes and draw on frame
-        left_eye_hull = cv2.convexHull(left_eye)
-        right_eye_hull = cv2.convexHull(right_eye)
-        cv2.drawContours(frame, [left_eye_hull], -1, (0, 0, 255), 1)
-        cv2.drawContours(frame, [right_eye_hull], -1, (0, 0, 255), 1)
-        # get EAR for both eyes
-        left_eye_aspect_ratio = eye_aspect_ratio(left_eye)
-        right_eye_aspect_ratio = eye_aspect_ratio(right_eye)
-        ear = (left_eye_aspect_ratio + right_eye_aspect_ratio) / 2.0
-
-
-
-    # show each frame for 1ms
-    cv2.imshow("Frame", frame)
-    key = cv2.waitKey(1)
+if __name__ == '__main__':
+    main()
