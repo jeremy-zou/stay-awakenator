c7b7800b6262e6e8981850403d75ab2b92b22910
jeremy-zou <jeremy.zou@gmail.com>
2019-12-18 01:26:47 -0500

    Added correct dependencies to setup.py

--- setup.py
+++ setup.py
@@ -3,10 +3,11 @@ from setuptools import setup
 setup(
     name='stay-awakenator',
     version='1.0',
-    packages=[''],
+    packages=[],
     url='https://github.com/jeremy-zou/stay-awakenator',
     license='',
     author='Jeremy Zou',
     author_email='',
-    description='', install_requires=['numpy', 'imutils', 'cmake', 'dlib', 'scipy', 'pigpio']
+    description='',
+    install_requires=['numpy', 'imutils', 'cmake', 'dlib', 'pigpio', 'scipy==1.3.3', 'opencv-python==3.4.6.27']
 )
