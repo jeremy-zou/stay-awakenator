929e2d5ecea254f910f922a1ebccf2662f95db2f
jeremy-zou <jeremy.zou@gmail.com>
2020-10-07 20:05:13 -0400

    Added start.sh

--- drowsiness_detector_config.py
+++ drowsiness_detector_config.py
@@ -1,5 +1,5 @@
 # Eye ratio threshold for closed eyes
-EYE_ASPECT_RATIO_THRESHOLD = 0.28
+EYE_ASPECT_RATIO_THRESHOLD = 0.20
 # The number of frames eyes are closed to be considered drowsy
 CLOSED_EYE_FRAMES_THRESHOLD = 10
 # Camera used for video stream
---
+++ start.sh
@@ -0,0 +1,3 @@
+source venv/bin/activate
+sudo pigpiod
+python3 drowsiness_detector.py
